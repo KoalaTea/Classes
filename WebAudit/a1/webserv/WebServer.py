414d036495301cb48c1f87daa8bd19a8ae503bf0
root <koalateac@gmail.com>
2017-01-29 21:55:43 -0500

    More status codes

--- WebAudit/a1/webserv/WebServer.py
+++ WebAudit/a1/webserv/WebServer.py
@@ -4,7 +4,7 @@ from Request import Request
 
 class WebServer(object):
 
-    def __init__(self, ip=0.0.0.0, port=80, config_file=None):
+    def __init__(self, ip="0.0.0.0", port=80, config_file=None):
         if(config_file):
             pass
         else:
@@ -12,36 +12,66 @@ class WebServer(object):
             self.port=port
             self.log = "/var/log/mywebserv/mywebserv.log"
             self.errlog = "/var/log/mywebserv/mywebserv.log"
-            self.methods = [ "PUT", "GET", "PUT", "DELETE", "CONNECT" ]
+            self.implemented_methods = [ "PUT", "GET", "POST", "DELETE", "CONNECT" ]
+            self.allowed_methods = [ "PUT", "GET", "POST", "DELETE", "CONNECT" ]
             self.code = []
             self.root = "/var/www"
 
 
-    def _response(self, method, resource, data=''):
-        if(method not in self.methods):
+    def _error_response(self, error):
+        if(error==405):
             if(os.is_file(self.root + "/405.html")):
                 response_data = "have 405.html"
             else:
                 response_data = "no 405.html"
-            response= "HTTP/1.1 405 METHOD NOT ALLOWED" + "\r\n"
-            response+="Allow: " + ", ".join(self.methods) + "\r\n"
+            response= "HTTP/1.1 405 Method Not Allowed" + "\r\n"
+            response+="Allow: " + ", ".join(self.allowed_methods) + "\r\n"
             response+="Content-Length: " + str(response_data.len()) + "\r\n"
-            response+="\r\n\r\n"
+            response+="\r\n"
             response+=response_data
+            response+="\r\n\r\n"
             return response
-        elif(method == "GET"):
-            if(any(isfile(join(self.root, i)) for i in listdir(self.root))):
-                pass
+
+        elif(error==404):
+            if(os.is_file(self.root + "/404.html")):
+                response_data = "have 405.html"
             else:
-                if(os.is_file(self.root + "/404.html")):
-                    response_data = "have 405.html"
-                else:
-                    response_data = "no 404.html"
-                response= "HTTP/1.1 404 FILE NOT FOUND" + "\r\n"
+                response_data = "no 404.html"
+            response= "HTTP/1.1 404 FILE NOT FOUND" + "\r\n"
+            response+="Content-Length: " + str(response_data.len()) + "\r\n"
+            response+="\r\n"
+            response+=response_data
+            response+="\r\n\r\n"
+            return response
+
+        elif(error==501):
+            response_data = "oops 501"
+            response= "HTTP/1.1 501 Method Not Implemented" + "\r\n"
+            response+="Allow: " + ",".join(self.allowed_methods) + "\r\n"
+            response+="Content-Length: " + str(response_data.len()) + "\r\n"
+            respones+="\r\n"
+            response+=response_data
+            response+="\r\n\r\n"
+            return response
+
+    def _response(self, method, resource, data=''):
+        if(method not in self.implemented_methods):
+            return self._error_response(501)
+        elif(method not in self.allowed_methods):
+            return self._error_response(405)
+        elif(method == "GET"):
+            if(any(isfile(join(self.root, resource)) for resource in listdir(self.root))):
+                #is a responses data suppossed to be \r\n per line?
+                with open(join(self.root, resource), 'r') as opened_file:
+                    response_data = opened_file.read
+                    opened_file.close()
+                response= "HTTP/1.1 200 OK" + "\r\n"
                 response+="Content-Length: " + str(response_data.len()) + "\r\n"
-                response+="\r\n\r\n"
+                response+="\r\n"
                 response+=response_data
-                return response
+                response+="\r\n\r\n"
+            else:
+                return self._error_response(404)
 
     def _handle_client(self, client_socket):
         full_data = ''
@@ -50,7 +80,7 @@ class WebServer(object):
             full_data = full_data + data
             if not data:
                 break
-        request = Request(client_data)
+        request = Request(full_data)
         method = request.get_method()
         resource = request.get_resource()
         data = request.data
